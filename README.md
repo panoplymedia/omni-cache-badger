35ae0309208ab99b8ab546e72eae1879544a6095
Jonathan Lehman <jonathan.lehman91@gmail.com>
2018-10-16 10:08:43 -0400

    Make naming less redundant

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Local Cache Badger
 
-A local cache based on [Badger](https://github.com/dgraph-io/badger).
+A [local cache](https://github.com/panoplymedia/local-cache) based on [Badger](https://github.com/dgraph-io/badger).
 
 ### Sample Usage
 
 ```go
 defaultTimeout := time.Minute
-badgerCache, err := NewCache(defaultTimeout, &badger.DefaultOptions, &badgercache.DefaultGCOptions)
+cache, err := NewCache(defaultTimeout, &badger.DefaultOptions, &badgercache.DefaultGCOptions)
 if err != nil {
   fmt.Println(err)
 }
 
 // open a connection to badger database
-conn, err := badgerCache.Open("my-database")
+conn, err := cache.Open("my-database")
 defer conn.Close()
 
 // write data to badger (uses defaultTimeout)
--- cache.go
+++ cache.go
@@ -8,16 +8,16 @@ import (
 	"github.com/dgraph-io/badger"
 )
 
-// BadgerCache contains options to connect to a badger database
+// Cache contains options to connect to a badger database
 // a TTL of 0 does not expire keys
-type BadgerCache struct {
+type Cache struct {
 	TTL    time.Duration
 	opts   *badger.Options
 	gcOpts *GarbageCollectionOptions
 }
 
-// BadgerConn is a connection to a badger database
-type BadgerConn struct {
+// Conn is a connection to a badger database
+type Conn struct {
 	TTL    time.Duration
 	db     *badger.DB
 	ticker *time.Ticker // for GC loop
@@ -29,8 +29,8 @@ type GarbageCollectionOptions struct {
 	DiscardRatio float64
 }
 
-// BadgerStats displays stats about badger
-type BadgerStats map[string]interface{}
+// Stats displays stats about badger
+type Stats map[string]interface{}
 
 // DefaultGCOptions are the default GarbageCollectionOptions
 var DefaultGCOptions = GarbageCollectionOptions{
@@ -38,10 +38,10 @@ var DefaultGCOptions = GarbageCollectionOptions{
 	DiscardRatio: 0.5,
 }
 
-// NewCache creates a new BadgerCache
-func NewCache(defaultTimeout time.Duration, opts *badger.Options, gcOpts *GarbageCollectionOptions) (*BadgerCache, error) {
+// NewCache creates a new Cache
+func NewCache(defaultTimeout time.Duration, opts *badger.Options, gcOpts *GarbageCollectionOptions) (*Cache, error) {
 	if defaultTimeout < time.Second && defaultTimeout > 0 {
-		return &BadgerCache{}, errors.New("TTL must be >= 1 second. Badger uses Unix timestamps for expiries which operate in second resolution")
+		return &Cache{}, errors.New("TTL must be >= 1 second. Badger uses Unix timestamps for expiries which operate in second resolution")
 	}
 	if opts == nil {
 		opts = &badger.DefaultOptions
@@ -50,17 +50,17 @@ func NewCache(defaultTimeout time.Duration, opts *badger.Options, gcOpts *Garbag
 		gcOpts = &DefaultGCOptions
 	}
 
-	return &BadgerCache{opts: opts, gcOpts: gcOpts, TTL: defaultTimeout}, nil
+	return &Cache{opts: opts, gcOpts: gcOpts, TTL: defaultTimeout}, nil
 }
 
 // Open opens a new connection to Badger
-func (c BadgerCache) Open(name string) (*BadgerConn, error) {
+func (c Cache) Open(name string) (*Conn, error) {
 	c.opts.Dir = name
 	c.opts.ValueDir = name
 
 	db, err := badger.Open(*c.opts)
 	if err != nil {
-		return &BadgerConn{}, err
+		return &Conn{}, err
 	}
 	// start a GC loop
 	ticker := time.NewTicker(c.gcOpts.Frequency)
@@ -73,30 +73,30 @@ func (c BadgerCache) Open(name string) (*BadgerConn, error) {
 			}
 		}
 	}(ticker, db)
-	return &BadgerConn{TTL: c.TTL, ticker: ticker, db: db}, nil
+	return &Conn{TTL: c.TTL, ticker: ticker, db: db}, nil
 }
 
 // Close closes the badger connection
-func (c *BadgerConn) Close() error {
+func (c *Conn) Close() error {
 	c.ticker.Stop()
 	return c.db.Close()
 }
 
 // Write writes data to the cache with the default cache TTL
-func (c *BadgerConn) Write(k, v []byte) error {
+func (c *Conn) Write(k, v []byte) error {
 	return c.WriteTTL(k, v, c.TTL)
 }
 
 // WriteTTL writes data to the cache with an explicit TTL
 // a TTL of 0 does not expire keys
-func (c *BadgerConn) WriteTTL(k, v []byte, ttl time.Duration) error {
+func (c *Conn) WriteTTL(k, v []byte, ttl time.Duration) error {
 	return c.db.Update(func(txn *badger.Txn) error {
 		return setWithTTL(txn, k, v, ttl)
 	})
 }
 
 // Read retrieves data for a key from the cache
-func (c *BadgerConn) Read(k []byte) ([]byte, error) {
+func (c *Conn) Read(k []byte) ([]byte, error) {
 	ret := []byte{}
 	err := c.db.View(func(txn *badger.Txn) error {
 		item, err := txn.Get(k)
@@ -110,9 +110,9 @@ func (c *BadgerConn) Read(k []byte) ([]byte, error) {
 }
 
 // Stats provides stats about the Badger database
-func (c *BadgerConn) Stats() map[string]interface{} {
+func (c *Conn) Stats() map[string]interface{} {
 	lsm, vlog := c.db.Size()
-	return BadgerStats{
+	return Stats{
 		"LSMSize":  lsm,
 		"VLogSize": vlog,
 	}
@@ -125,7 +125,7 @@ func (c *BadgerConn) Stats() map[string]interface{} {
 // added/modified since the last invocation of DB.Backup()
 //
 // This can be used to backup the data in a database at a given point in time.
-func (c *BadgerConn) Backup(w io.Writer, since uint64) (upto uint64, err error) {
+func (c *Conn) Backup(w io.Writer, since uint64) (upto uint64, err error) {
 	return c.db.Backup(w, since)
 }
 
@@ -135,7 +135,7 @@ func (c *BadgerConn) Backup(w io.Writer, since uint64) (upto uint64, err error) 
 //
 // DB.Load() should be called on a database that is not running any other
 // concurrent transactions while it is running.
-func (c *BadgerConn) Load(r io.Reader) error {
+func (c *Conn) Load(r io.Reader) error {
 	return c.db.Load(r)
 }
 
